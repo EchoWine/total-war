b2b4236a48ffc0ba001350ac729156fe17852996
EchoWine <dimitri.who@gmail.com>
2016-10-06 23:25:15 +0200

    Added config

--- src/main/java/io/github/echowine/totalwar/TotalWarPlugin.java
+++ src/main/java/io/github/echowine/totalwar/TotalWarPlugin.java
@@ -13,38 +13,113 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.args.GenericArguments;
 
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+import java.nio.file.Path;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
+import java.net.URL;
 
 @Plugin(id = "totalwar", name = "Total War Plugin", version = "1.0", description = "A sexy description")
 public class TotalWarPlugin {
 	
 	@Inject
-	private Logger logger;
+	@DefaultConfig(sharedRoot = true)
+	private Path default_config;
+
+	@Inject
+	@DefaultConfig(sharedRoot = true)
+	private ConfigurationLoader<CommentedConfigurationNode> config_manager;
+
+	@Inject
+	@ConfigDir(sharedRoot = false)
+	private Path private_config_dir;
 	
+	@Inject
+	private Logger logger;
+
+	/**
+	 * Retrieve the logger
+	 *
+	 * @return 
+	 */
 	public Logger getLogger() {
 	    return logger;
 	}
 	
-	@Inject
-	public TotalWarPlugin(Logger logger) {
+	/**
+	 * Retrieve the config path
+	 *
+	 * @return 
+	 */
+	public Path getConfigPath() {
+	    return this.private_config_dir;
+	}
+
+
+	/**
+	 * Constructor
+	 */
+	@Inject public TotalWarPlugin(Logger logger) {
 	    this.logger = logger;
 	}
 	
-	@Listener
-    public void onServerStart(GameStartedServerEvent event) {
+	/**
+	 * Called when server start
+	 * 
+	 * @param event
+	 */
+	@Listener public void onServerStart(GameStartedServerEvent event) {
 
-		getLogger().info("Total War v1.0");
+		try{
+
+			getLogger().info("Total War v1.0");
+			
+			ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader.builder().setPath(getConfigPath()).build();
+			ConfigurationNode cfg;
+			
+		    cfg = loader.load();
+		    
+		    if(cfg.getNode("database","hostname").getString() == null){
+
+			    URL jar_config = this.getClass().getResource("config.conf");
+			    ConfigurationLoader<CommentedConfigurationNode> jar_loader = HoconConfigurationLoader.builder().setURL(jar_config).build();
+			    
+			    cfg = jar_loader.load();
+			    loader.save(cfg);
+			    
+		    }
+
+		    DataBase.setLogger(logger);
+			DataBase.connect(
+				cfg.getNode("database","hostname").getString(),
+				cfg.getNode("database","username").getString(),
+				cfg.getNode("database","password").getString(),
+				cfg.getNode("database","database").getString()
+			);
+			
+			Faction.ini();
+			
+			this.registerCommands(event);
+			
+		}catch(Exception e) {
 		
-		DataBase.setLogger(logger);
-		DataBase.connect("localhost","root","","totalwar");		
-		Faction.ini();
+			getLogger().error(e.getMessage());
+		}
 		
-		this.registerCommands(event);
+		
 	}
 	
 	/**
